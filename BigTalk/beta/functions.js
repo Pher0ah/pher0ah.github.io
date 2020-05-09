f044000d5dad00bc4906b9d27bb22b22f3ab6c08
Hany <pheroah@gmail.com>
2020-05-10 01:03:57 +1000

    Use JSON

--- BigTalk/beta/functions.js
+++ BigTalk/beta/functions.js
@@ -1,83 +1,42 @@
-//Create a counter for how many cards we have used
-var counter = 0;
+// Global Variables Section
+//
+var counter = 0;                //A counter for how many cards we have used
+const BTQuestions = [];         //The questions to be used
 
 const logoLocations = {BigTalk : 'https://www.makebigtalk.com/wp-content/themes/blankslate/img/big-talk-logo-pos.svg',
                        Puzzle  : 'https://www.stickpng.com/assets/images/5a4613ddd099a2ad03f9c994.png',
-                       Islamic : 'https://i.pinimg.com/originals/75/2a/0d/752a0dc0a3cd15ae456a8bb91895c8b0.png'}
-
-//Shuffle The Questions
-shuffleQuestions(BTQuestions);
+                       Islamic : 'https://i.pinimg.com/originals/75/2a/0d/752a0dc0a3cd15ae456a8bb91895c8b0.png'};
 
 
-//Setup Event for window resize
-//
+                       
+// //Create Cards First Time Around (NOT WORKING)
+// document.body.addEventListener('load', createCards);
+
+
+//Listen for Resize Window Event & move Cards around
 window.addEventListener('resize',redistributeCards);
 
-function redistributeCards(){
-  const allCards = document.querySelectorAll('.container');
-  allCards.forEach(card => {
-    let posX = card.computedStyleMap().get('left').value;
-    deltaX = posX >= (window.innerWidth - 300) ? (Math.floor(Math.random() * (window.innerWidth - 300))) - posX : 0;
 
-    let posY = card.computedStyleMap().get('top').value;
-    deltaY = posY >= (window.innerHeight - 200) ? (Math.floor(Math.random() * (window.innerHeight- 200))) - posY : 0;
-
-    const ct = card.computedStyleMap().get('transform'); 
-
-    let currentRotation = ct[ct.length -1].angle.value;
-    card.style.transform = `translate(${deltaX}px, ${deltaY}px)  rotate(${currentRotation}deg)`;
-    // card.style.left = `${posX}px`;
-    // card.style.top = `${posY}px`;
-  });
-}
-
-function reshuffleCards(){
-  const allCards = document.querySelectorAll('.container');
-  allCards.forEach(moveCard);
-}
-
-function moveCard(card, index){
-  const posY = Math.floor(Math.random() * (window.innerHeight- 200));
-  const posX = Math.floor(Math.random() * (window.innerWidth - 300));
-
-  card.style.left = `${posX}px`;
-  card.style.top = `${posY}px`;
-}
-
-// // Description: Flip the card over & Setup the new question
-// //
-// function flipCard() {
-//   //Change the question
-//   if (this.className !== "card flipped") {
-//     //Add Question
-//     this.children[1].textContent = BTQuestions[counter][2];
-
-//     //Check if we have reached the end of the array
-//     if (++counter >= BTQuestions.length) {
-//       shuffleQuestions(BTQuestions);
-//       counter = 0
-//     }
-//     //Add Question Logo for next question to the back
-//     this.children[0].style.backgroundImage = `url(${logoLocations[BTQuestions[counter][0]]})`;
-//   }
-
-//   //flip the card over
-//   this.classList.toggle("flipped");
-// }
-
-// Description: Flip the card over
+// Description: Load Questions from JSON File
 //
-function flipCard() {
-  //bring the card to the front
-  const selectedCard = this.parentElement;
-  //selectedCard.style.transform = `translate3d(0px, 0px, -1000px)`;
-  // console.log(selectedCard.computedStyleMap.zIndex);
-  // document.body.removeChild(selectedCard);
-  // document.body.appendChild(selectedCard);
-
-  //flip the card over
-  this.classList.toggle("flipped");
+//
+async function loadQuestions(){
+  //Read questions from JSON File
+  const response = await fetch('./questions.json')
+  
+  //Check if we got the file ok
+  if(!response.ok){
+    throw new Error(`Couldn't read questions from server (ERROR:${response.status})`);
+  }
+  
+  //Convert response into JSON
+  const data = await response.json()
+  
+  //Parse the questions into the BTQuestions variable
+  data.questions.forEach((question) => BTQuestions.push(question));
+  return BTQuestions.length;
 }
+
 
 // Description: Shuffle Array Function
 // Reference  :https://bost.ocks.org/mike/shuffle/
@@ -87,15 +46,27 @@ function shuffleQuestions(array) {
     let j = Math.floor(Math.random() * (i+1));
     [array[i],array[j]] = [array[j],array[i]];
   }
+
+  return Promise.resolve(array);
 }
 
-// Description: Create Lots of Cards
+
+// Description: Load Questions and Create the Cards
 //
-function createCards(){
+//
+async function createCards(){
+  //Load the Questions
+  await loadQuestions();
+ 
+  //Shuffle The Questions
+  console.log(BTQuestions.length);
+  shuffleQuestions(BTQuestions);
+  
+  //Loop through all questions and create card DIVs for them
   for(let cardInfo of BTQuestions){
-    const cardType = cardInfo[0];
-    const cardCat  = cardInfo[1];
-    const cardQ  = cardInfo[2];
+    const cardType = cardInfo.type;
+    const cardCat  = cardInfo.category;
+    const cardQ  = cardInfo.question;
 
     //Create the container for the card
     const  containerDiv = document.createElement('div');
@@ -127,11 +98,80 @@ function createCards(){
     cardBack.classList.add("back");
     cardBack.innerText = `${cardQ}`;
     cardDiv.appendChild(cardBack);
+  }
+}
 
 
-// //Setup the first card on the screen
-// const firstCard = document.querySelector('.card');
-// firstCard.children[0].style.backgroundImage = `url(${logoLocations[BTQuestions[counter][0]]})`;
-// firstCard.addEventListener('click',function(){flipCard(firstCard)});
+function redistributeCards(){
+  const allCards = document.querySelectorAll('.container');
+  allCards.forEach(card => {
+    let posX = card.computedStyleMap().get('left').value;
+    deltaX = posX >= (window.innerWidth - 300) ? (Math.floor(Math.random() * (window.innerWidth - 300))) - posX : 0;
+
+    let posY = card.computedStyleMap().get('top').value;
+    deltaY = posY >= (window.innerHeight - 200) ? (Math.floor(Math.random() * (window.innerHeight- 200))) - posY : 0;
+
+    const ct = card.computedStyleMap().get('transform'); 
+
+    let currentRotation = ct[ct.length -1].angle.value;
+    card.style.transform = `translate(${deltaX}px, ${deltaY}px)  rotate(${currentRotation}deg)`;
+  });
+}
+
+
+//
+//
+//
+function reshuffleCards(){
+  const allCards = document.querySelectorAll('.container');
+  allCards.forEach(moveCard);
+}
+
+
+//
+//
+//
+function moveCard(card, index){
+  const posY = Math.floor(Math.random() * (window.innerHeight- 200));
+  const posX = Math.floor(Math.random() * (window.innerWidth - 300));
+
+  card.style.left = `${posX}px`;
+  card.style.top = `${posY}px`;
+}
+
+// // Description: Flip the card over & Setup the new question
+// //
+// function flipCard() {
+//   //Change the question
+//   if (this.className !== "card flipped") {
+//     //Add Question
+//     this.children[1].textContent = BTQuestions[counter][2];
+
+//     //Check if we have reached the end of the array
+//     if (++counter >= BTQuestions.length) {
+//       shuffleQuestions(BTQuestions);
+//       counter = 0
+//     }
+//     //Add Question Logo for next question to the back
+//     this.children[0].style.backgroundImage = `url(${logoLocations[BTQuestions[counter][0]]})`;
+//   }
+
+//   //flip the card over
+//   this.classList.toggle("flipped");
+// }
+
+// Description: Flip the card over
+//
+//
+function flipCard() {
+  //bring the card to the front
+  const selectedCard = this.parentElement;
+
+  if(this.className !== "card flipped"){
+    //flip the card over
+    this.classList.toggle("flipped");
+  }else{
+    const container = this.parentElement;
+    container.parentElement.removeChild(container);
   }
 }
