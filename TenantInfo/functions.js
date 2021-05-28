a67e84f6a27e5cc33c6f2314e28687bda81c47e9
Hany E <pheroah@gmail.com>
2021-05-28 15:48:33 +1000

    Creation

---
+++ TenantInfo/functions.js
@@ -0,0 +1,59 @@
+//Comment
+
+//Read a JSON File
+function readTextFile(file, callback) {
+  var rawFile = new XMLHttpRequest();
+  rawFile.overrideMimeType("application/json");
+  rawFile.open("GET", file, true);
+  rawFile.onreadystatechange = function() {
+      if (rawFile.readyState === 4 && rawFile.status == "200") {
+          callback(rawFile.responseText);
+      }
+  }
+  rawFile.send(null);
+}
+
+//Load Information
+function LoadData(){
+  readTextFile("./tenantInfo.js", function(text){
+    var data = JSON.parse(text);
+    console.log(data);
+  });
+}
+
+
+//=============================
+//flip the card over
+function flipCard(card) {
+  //Change the question (for now)
+  if (card.className !== "card flipped") {
+    card.children[1].textContent = BTQuestions[counter++][2];
+    if (counter === BTQuestions.length) {
+      BTQuestions = shuffle(BTQuestions);
+      counter = 0
+    }
+  }
+  card.classList.toggle("flipped");
+}
+
+
+// Description: Shuffle Array Function
+// Reference  :https://bost.ocks.org/mike/shuffle/
+//
+function shuffle(array) {
+  let m = array.length, t, i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
+}
