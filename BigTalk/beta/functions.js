ce312f71460f3463c1a9178a29f24046aef57bc8
Hany Elkady <hany@idareu.org.au>
2022-11-30 21:52:17 +1100

    Automate Colours

--- BigTalk/beta/functions.js
+++ BigTalk/beta/functions.js
@@ -38,3 +38,35 @@ function shuffle(array) {
 
   return array;
 }
+
+function resizeCard() {
+  theHeight = window.innerHeight;
+  theWidth = window.innerWidth;
+
+  if(theWidth > theHeight){
+    theCardHeight = (theHeight / 5)*4;
+    theCardWidth  = (theCardHeight / 2)*3;
+    theFontSize = theCardHeight/15;
+  }else{
+    theCardWidth = (theWidth / 5) * 4;
+    theCardHeight = (theCardWidth / 3)*2;
+    theFontSize = theCardWidth/15;
+  };
+
+  //Change Card Dimensions & Font Size
+  document.getElementsByClassName('container')[0].style.width = `${theCardWidth}px`;
+  document.getElementsByClassName('container')[0].style.height = `${theCardHeight}px`;
+  document.getElementsByClassName('container')[0].style.borderRadius = `${theCardHeight/10}px`;
+
+  document.getElementsByClassName('back')[0].style.fontSize = `${theFontSize}px`;
+  document.getElementsByClassName('back')[0].style.font.color = `#${invertHex(cardColour)}`;
+  //document.getElementsByClassName('card')[0].style.backgroundColor = `#${cardColour}`;
+  document.getElementsByClassName('back')[0].style.color = '#FFFFFF !important';
+};
+
+//Find the inverse of a colour
+//Ref: https://stackoverflow.com/questions/35969656/how-can-i-generate-the-opposite-color-according-to-current-color
+//
+function invertHex(hex) {
+  return (Number(`0x1${hex}`) ^ 0xFFFFFF).toString(16).substr(1).toUpperCase()
+}
