86bcf204446445b2cad97ea778affa6ddbb92cbb
Hany Elkady <hany@idareu.org.au>
2022-12-06 21:29:15 +1100

    more cards

--- BigTalk/beta/Couples.js
+++ BigTalk/beta/Couples.js
@@ -1,7 +1,7 @@
-const gameTitle = "Bad Choices";
-const tableColour = '3399ff';
-const cardColour = '000099';
-const cardLogo = `url("https://cdn.shopify.com/s/files/1/0045/5658/7057/t/3/assets/logo_200x.png")`;
+const gameTitle = "Couples Quiz";
+const tableColour = '22ffff';
+const cardColour = '550000';
+const cardLogo = `url("https://www.egynomics.com/Images/couples-logo.png")`;
 
 const theQuestions = [
   ["General","As a kid, what scared you about growing up?"],
