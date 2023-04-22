eae8e6dc6277e4a468d8461243633f89f2f41edf
Hany Elkady <hany@idareu.org.au>
2023-04-22 18:48:44 +1000

    separate files

---
+++ GPhotos/script.js
@@ -0,0 +1,126 @@
+const API_KEY = 'AIzaSyBqYyTlQoxWZpEYogPt9cHAJoIvlZySoko';
+const CLIENT_ID = '872435311738-6o0o4bmu4arcoo9m58oh0n7e0ovrr94s.apps.googleusercontent.com';
+const SCOPES = 'https://www.googleapis.com/auth/photoslibrary.readonly';
+
+function init() {
+  gapi.load('client:auth2', initClient);
+}
+
+function initClient() {
+  gapi.client.init({
+    apiKey: API_KEY,
+    clientId: CLIENT_ID,
+    scope: SCOPES
+  }).then(() => {
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+  });
+}
+
+function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {
+    loadPhotos();
+  } else {
+    gapi.auth2.getAuthInstance().signIn();
+  }
+}
+
+const PAGE_SIZE = 10;
+let currentPage = 0;
+let currentPhotos = [];
+
+async function loadPhotos() {
+  try {
+    const albums = await getAlbums();
+    const photos = await getAllPhotos();
+
+    currentPhotos = photos.map(photo => {
+      const album = albums.find(album => album.id === photo.albumId);
+      return {
+        url: photo.baseUrl,
+        album: album ? album.title : 'No Album'
+      };
+    });
+
+    displayPage(currentPage);
+  } catch (error) {
+    displayError(error.message);
+  }
+}
+
+async function getAlbums() {
+  const response = await gapi.client.photoslibrary.albums.list();
+  return response.result.albums || [];
+}
+
+async function getAllPhotos() {
+  let photos = [];
+  let nextPageToken;
+
+  do {
+    const response = await gapi.client.photoslibrary.mediaItems.list({
+      pageSize: 100,
+      pageToken: nextPageToken
+     });
+
+     photos = photos.concat(response.result.mediaItems || []);
+     nextPageToken = response.result.nextPageToken;
+  } while (nextPageToken);
+
+  return photos;
+}
+
+function displayPage(page) {
+  const start = page * PAGE_SIZE;
+  const end = start + PAGE_SIZE;
+  const pageData = currentPhotos.slice(start, end);
+
+  updateTable(pageData);
+
+  const prevButton = document.getElementById('prev-page');
+  const nextButton = document.getElementById('next-page');
+  prevButton.disabled = page === 0;
+  nextButton.disabled = end >= currentPhotos.length;
+}
+
+function previousPage() {
+  if (currentPage > 0) {
+    currentPage--;
+    displayPage(currentPage);
+  }
+}
+
+function nextPage() {
+  const totalPages = Math.ceil(currentPhotos.length / PAGE_SIZE);
+  if (currentPage < totalPages - 1) {
+    currentPage++;
+    displayPage(currentPage);
+  }
+}
+
+function updateTable(data) {
+  const tbody = document.querySelector('#photos-table tbody');
+  tbody.innerHTML = '';
+
+  data.forEach(rowData => {
+    const row = document.createElement('tr');
+    const imgCell = document.createElement('td');
+    const img = document.createElement('img');
+
+    img.src = rowData.url;
+    img.width = 100;
+    imgCell.appendChild(img);
+    row.appendChild(imgCell);
+
+    const albumCell = document.createElement('td');
+
+    albumCell.innerText = rowData.album;
+    row.appendChild(albumCell);
+    tbody.appendChild(row);
+  });
+}
+
+function displayError(message) {
+  const errorMessageElement = document.getElementById('error-message');
+  errorMessageElement.textContent = message;
+}
