db77af03d1c0f9726a91950d5558f930d116a7a0
Pher0ah <helkady@hotmail.com>
2023-05-21 20:17:01 +1000

    remove cookie based storage

--- b2c/functions.js
+++ b2c/functions.js
@@ -1,59 +1,37 @@
 window.onload = function() {
-  loadCookies();
+  loadDetails();
 }
 
 function saveDetails() {
   var tenantID = document.getElementById('tenantID').value;
   var username = document.getElementById('username').value;
-  var d = new Date();
-  d.setTime(d.getTime() + (24*60*60*1000));
-  var expires = "expires="+ d.toUTCString();
-  document.cookie = "tenant_" + tenantID + "=" + username + ";" + expires + ";path=/";
-  loadCookies();
+  localStorage.setItem('tenant_' + tenantID, username);
+  loadDetails();
 }
 
-function loadCookies() {
-  var cookies = document.cookie.split('; ');
-  var tenantDropdown = document.getElementById('tenantDropdown');
-  tenantDropdown.innerHTML = '';
-
-  for (var i = 0; i < cookies.length; i++) {
-      var cookiePair = cookies[i].split('=');
-      if (cookiePair[0].startsWith('tenant_')) {
-          var tenantID = cookiePair[0].substring(7);
-          var option = document.createElement('option');
-          option.value = tenantID;
-          option.textContent = tenantID;
-          tenantDropdown.appendChild(option);
+function loadDetails() {
+  var select = document.getElementById('tenantDropdown');
+  select.options.length = 0; // clear out existing options
+  for (var i = 0; i < localStorage.length; i++){
+      var key = localStorage.key(i);
+      if(key.startsWith('tenant_')) {
+          var tenantID = key.substring('tenant_'.length);
+          var opt = document.createElement('option');
+          opt.value = tenantID;
+          opt.innerHTML = tenantID;
+          select.appendChild(opt);
       }
   }
 }
 
 function displayUsername(tenantID) {
-  var username = getCookie("tenant_" + tenantID);
-  document.getElementById('usernameDisplay').textContent = username;
-}
-
-function getCookie(cname) {
-  var name = cname + "=";
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
-  for(var i = 0; i <ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-  }
-  return "";
+  var username = localStorage.getItem('tenant_' + tenantID);
+  document.getElementById('usernameDisplay').innerText = username;
 }
 
 function deleteDetails() {
   var tenantID = document.getElementById('tenantDropdown').value;
-  if (tenantID !== '') {
-      document.cookie = "tenant_" + tenantID + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
-      loadCookies();
-  }
+  localStorage.removeItem('tenant_' + tenantID);
+  loadDetails();
+}
 }
